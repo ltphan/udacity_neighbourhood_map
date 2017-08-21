c8e231d214cf64b0c7e1144678e23c4b692e9d1f
Linh Phan <phanthuclinh@gmail.com>
2017-08-21 11:07:07 -0700

    refactored and able to get locations to pop up

--- js/app.js
+++ js/app.js
@@ -3,23 +3,23 @@
 var locations = [
   {
       name: 'Ajisai Sushi Restaurant',
-      location: {lat: 49.233934, lng: -123.1536816}
+      position: {lat: 49.233934, lng: -123.1536816}
   },
   {
       name: 'Doplan Korean Restuarant',
-      location: {lat: 49.1856592, lng: -123.1292477}
+      position: {lat: 49.1856592, lng: -123.1292477}
   },
   {
       name: 'Nightingale Restaurant',
-      location: {lat: 49.2874341, lng: -123.1175606}
+      position: {lat: 49.2874341, lng: -123.1175606}
   },
   {
       name: 'Hoi An Cafe',
-      location: {lat: 49.2388517, lng: -123.0652127}
+      position: {lat: 49.2388517, lng: -123.0652127}
   },
   {
       name: 'Jam Cafe',
-      location: {lat: 49.2802619, lng: -123.1096419}
+      position: {lat: 49.2802619, lng: -123.1096419}
   }
 ]
 
@@ -30,37 +30,31 @@ var markers = [];
 var infoWindow;
 
 function initMap() {
-  console.log("map printed");
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 49.2827291, lng: -123.1207375},
     zoom: 13,
     mapTypeControl: false
   });
+  setMarkers(locations, vm);
 }
 
-
-var Location = function(data) {
-  var self = this;
-  self.name = data.name;
-  self.lat = data.lat;
-  self.lng = data.lng;
-  self.fourSquareID = data.fourSquareID;
-}
-
-function setMarkers(locations) {
-  locations.forEach(function(location) {
-    var positions = {lat: location.lat, lng: location.lng};
+function setMarkers(locations,vm) {
+  locations.forEach(function(location, i) {
+    console.log(location);
+    var position = {lat: location.position.lat, lng: location.position.lng};
     var name = location.name;
-    location.infoWindow = new google.maps.InfoWindow();
-    location.marker = new google.maps.Makrer({
+   // location.infoWindow = new google.maps.InfoWindow();
+    marker = new google.maps.Marker({
       map: map,
       position: position,
       title: name,
       animation: google.maps.Animation.DROP
     })
 
+   vm.listingList()[i].marker = marker;
+
     markers.push(location.marker);
-    location.marker.addListener('click', function() {
+    marker.addListener('click', function() {
       populateInfoWindow(this, largeInfoWindow);
     })
   });
@@ -71,10 +65,7 @@ function populateInfoWindow(marker,infoWindow) {
   // foursquare information
   var CLIENT_ID = 'F3T5F3US0WH4QBJTBSGSA2WIMCTUIXPECQK2RZXRQ01N1QW4';
   var CLIENT_SECRET = 'CACMXCC2LGMQKBIJCDJJD3ZRGKIOPFVFKPQC2IXWS3NFNJUV';
-  var url = 'https://api.foursquare.com/v2/venues/venues/search?ll=' + ;
-
-
-
+ // var url = 'https://api.foursquare.com/v2/venues/venues/search?ll=' + ;
   
 }
 
@@ -82,8 +73,12 @@ function populateInfoWindow(marker,infoWindow) {
 // View
 
 var Listing = function(data) {
-  this.name = ko.observable(data.name);
-  this.location = ko.observable(data.location);
+  var self = this;
+  this.location = data.location;
+  self.name = data.name;
+  self.lat = data.position.lat;
+  self.lng = data.position.lng;
+  self.fourSquareID = data.fourSquareID;
 }
 
 var ViewModel = function() {
