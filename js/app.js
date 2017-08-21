513822f25cd4f42f21d17c6e74bc363b533af4c8
Linh Phan <phanthuclinh@gmail.com>
2017-08-21 16:04:21 -0700

    able to centre the values to see all the markers

--- js/app.js
+++ js/app.js
@@ -31,11 +31,12 @@ var infoWindow;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 49.2827291, lng: -123.1207375},
-    zoom: 13,
+    center: {lat: 49.245, lng: -123.1207375},
+    zoom: 12,
     mapTypeControl: false
   });
   setMarkers(locations, vm);
+  infowindow = new google.maps.InfoWindow();
  
 }
 
@@ -51,14 +52,11 @@ function setMarkers(locations,vm) {
       animation: google.maps.Animation.DROP
     })
     vm.locationList()[i].marker = marker;
-    //var bounds = new google.maps.LatLngBounds();
 
     markers.push(location.marker);
-    //bounds.extend(location.marker.position);
     marker.addListener('click', function() {
       populateInfoWindow(this, infoWindow);
     })
-    //map.fitBounds(bounds);
   });
   
 }
@@ -87,7 +85,7 @@ function populateInfoWindow(marker, infoWindow) {
 
 var Location = function(data) {
   var self = this;
-  this.location = data.location;
+  self.location = data.location;
   self.name = data.name;
   self.lat = data.position.lat;
   self.lng = data.position.lng;
